9fd41f39f52d6283259f2651ae8d86cbc9a77b93
OldaranT <timbaak@hotmail.com>
2018-04-03 16:29:21 +0200

    Added Graph

---
+++ RealmOfCollection/RealmOfCollection/Graphs/IGraph.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealmOfCollection.Graphs
+{
+    interface IGraph
+    {
+        Vertex GetVertex(String name);
+        void AddEdge(String source, String dest, double cost);
+        string ToString();
+
+    }
+}
